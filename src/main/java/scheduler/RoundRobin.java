4db26d6ae3b9f6c2737e384329c642d94cc45b8d
s-blanco-dev <santiago.blancoc@correo.ucu.edu.uy>
2025-06-02 17:01:39 -0300

    Round Robin era una bazofia

--- src/main/java/scheduler/RoundRobin.java
+++ src/main/java/scheduler/RoundRobin.java
@@ -20,39 +20,43 @@ public class RoundRobin implements IAlgoritmo {
         Queue<Proceso> colaListos = new LinkedList<>();
         int tiempoActual = 0;
 
-        /* Mientras existan procesos esperando en la cola */
         while (!colaProcesos.isEmpty() || !colaListos.isEmpty()) {
-            while (!colaProcesos.isEmpty() && colaProcesos.peek().getLlegada() == tiempoActual) {
+
+            // Agregar todos los procesos que ya llegaron a la cola
+            while (!colaProcesos.isEmpty() && colaProcesos.peek().getLlegada() <= tiempoActual) {
                 colaListos.add(colaProcesos.poll());
             }
 
             if (!colaListos.isEmpty()) {
                 Proceso actual = colaListos.poll();
-                int quantumRestante = this.quantum;
+                int tiempoEjecutado = 0;
 
-                // Ejecuta el proceso durante el quantum o hasta que termine su ráfaga
-                while (quantumRestante > 0 && actual.getRafaga() > 0) {
+                // Ejecutar hasta terminar quantum o hasta que el proceso se derrita
+                while (tiempoEjecutado < quantum && actual.getRafaga() > 0) {
                     listaFinal.add(actual);
                     actual.decrementar();
+                    tiempoEjecutado++;
                     tiempoActual++;
-                    quantumRestante--;
+
+                    // Me fijo si llegó algo nuevo
+                    while (!colaProcesos.isEmpty() && colaProcesos.peek().getLlegada() <= tiempoActual) {
+                        colaListos.add(colaProcesos.poll());
+                    }
                 }
 
+                // Si no terminó, vuelve a la cola
                 if (actual.getRafaga() > 0) {
-                    colaProcesos.add(actual);
-                }
-
-                if (actual.getRafaga() == 0) {
-                    colaListos.remove(actual); // elimino proceso completado
+                    colaListos.add(actual);
                 }
             } else {
-                /* Si no hay procesongo listo, avanza el tie
-                mpo */
                 tiempoActual++;
             }
         }
+
         return listaFinal;
     }
+
+
 
     private Proceso obtenerMasChico(Queue<Proceso> cola) {
         Proceso chicuelo = cola.stream()
