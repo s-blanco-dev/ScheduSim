f6e5f5f923c50d2399d8f4a23383471dc39ee162
Martin <mujica0106@gmail.com>
2025-06-13 18:36:24 -0300

    Se agregan mas procesos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,9 +19,18 @@ public class Main {
         System.out.println(scheduler.schedule(cola));*/
 
         List<Proceso> procesos = List.of(
-                new Proceso("P1", 0, 4, 3),
+                new Proceso("P1", 0, 4, 1),
                 new Proceso("P2", 1, 2, 2),
-                new Proceso("P3", 3, 6, 1)
+                new Proceso("P3", 3, 6, 3),
+                new Proceso("P4", 8, 4, 4),
+                new Proceso("P5", 12, 2, 5),
+                new Proceso("P6", 16, 6, 6),
+                new Proceso("P7", 19, 4, 7),
+                new Proceso("P8", 22, 2, 8),
+                new Proceso("P9", 23, 6, 1),
+                new Proceso("P10", 25, 4, 5),
+                new Proceso("P11", 27, 2, 6),
+                new Proceso("P12", 30, 6, 9)
         );
 
         Scheduler schedule = new Prioridad();
@@ -32,6 +41,5 @@ public class Main {
         // TABLA DE TIEMPO
         // PROCESOS EN TIEMPO REAL, AL AZAR
         //
-
     }
 }
